1d36a7bbcf255593dd43724ec5582891988d7f5a
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-04-11 13:52:19 -0700

    added a timer to the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,57 @@ var startQuizEl = document.querySelector(".start-quiz"); //.start-quiz is class 
 var welcomeScreenEl = document.querySelector(".welcome-screen");
 var questionScreenEL = document.querySelector(".question-screen");
 
-var questionIndex = 0;
 
+//Timer
+var timeLeft = 75;
+var countDownEl = document.getElementById("countdown");
+var countdownTimer = setInterval(function() {
+    timeLeft--;
+    countDownEl.innerHTML = timeLeft;
+    if (timeLeft <= 0) {
+        clearInterval(countdownTimer);
+        alert("Time's up!!");
+    }
+}, 1000);
+
+
+var questionIndex = 0;
 
 const questions = [
     {
-        question: 'What does HTML stand for?',
-        optionA: 'asda',
-        optionB: 'asda',
-        optionC: 'asda',
-        optionD: 'asda',
+        question: 'Commonly used data types DO Not Incldue:?',
+        optionA: '1. strings',
+        optionB: '2. booleans',
+        optionC: '3. alerts',
+        optionD: '4. numbers',
     },
     {
-        question: 'What does HTML stand for?',
-        optionA: 'asda',
-        optionB: 'asda',
-        optionC: 'asda',
-        optionD: 'asda',
+        question: 'The condition in an if / else statement is enclosed with ___?',
+        optionA: '1. quotes',
+        optionB: '2. curly brackets',
+        optionC: '3. parenthesis',
+        optionD: '4. square brackets',
     },
     {
-        question: 'What does HTML stand for?',
-        optionA: 'asda',
-        optionB: 'asda',
-        optionC: 'asda',
-        optionD: 'asda',
+        question: 'Arrays in JavaScript can be used ot store ___.',
+        optionA: '1. numbers and strings',
+        optionB: '2. other arrays',
+        optionC: '3. booleans',
+        optionD: '4. all of the above',
     },
     {
-        question: 'What does HTML stand for?',
-        optionA: 'asda',
-        optionB: 'asda',
-        optionC: 'asda',
-        optionD: 'asda',
+        question: 'String values must be enclosed within ____ when being assigned to variable.',
+        optionA: '1. commas',
+        optionB: '2. curly brackets',
+        optionC: '3. quotes',
+        optionD: '4. parenthesis',
     },
     {
-        question: 'What does HTML stand for?',
-        optionA: 'asda',
-        optionB: 'asda',
-        optionC: 'asda',
-        optionD: 'asda',
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        optionA: '1. JavaScript',
+        optionB: '2. terminal/bash',
+        optionC: '3. for loops',
+        optionD: '4. console.log',
     }
 ]
 
@@ -49,6 +62,7 @@ function startQuiz() { //created a function that starts the quiz
     questionScreenEL.classList.remove('hide')
 }
 
+//function 
 
 startQuizEl.addEventListener('click', startQuiz) //var will have an event listener for the click and will call function created to hide
 
